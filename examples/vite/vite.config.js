1e8661befc209491b023164908402e34068b2f79
Federico <65908512+coval3nte@users.noreply.github.com>
2026-03-14 03:28:07 +0000

    chore: add browser example

---
+++ examples/vite/vite.config.js
@@ -0,0 +1,15 @@
+import wasm from "vite-plugin-wasm";
+import { defineConfig } from 'vite';
+export default defineConfig({
+  plugins: [
+   wasm(),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
+  },
+});
