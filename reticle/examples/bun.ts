e7ca355390138b2f64afeca950ac33abcff236c7
Pietro T <17928339+BRA1L0R@users.noreply.github.com>
2026-05-13 10:03:20 +0000

    attested fetch

--- reticle/examples/bun.ts
+++ reticle/examples/bun.ts
@@ -1,14 +1,13 @@
 // import a from "../"
 
-import { ClientBuilder, QueryParams, GatewayError } from "../pkg/prem_rs.js";
-
+import { ClientBuilder, QueryParams, GatewayError } from "../pkg/reticle";
 
 try {
     let client = await new ClientBuilder("https://gateway.prem.io/").build();
-    let query_params = new QueryParams().with("model", "modelmodel");
+    // let query_params = new QueryParams().with("model", "modelmodel");
 
-    await client.attest(query_params);
-} catch (e) {
+    await client.attest();
+} catch (e: any) {
     if (e.kind instanceof GatewayError)
         console.log("Gateway error:", e.kind.message);
     else
